fbde9fc3298624fd2da5d6e4fb8e202aacc86672
KiritoCheenu <61424879+KiritoCheenu@users.noreply.github.com>
2021-02-28 10:53:43 +0530

    gradle error resolved

--- lib/screens/otp.dart
+++ lib/screens/otp.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pin_put/pin_put.dart';
 
 class OTPScreen extends StatefulWidget {
   final String phone;
+
   OTPScreen(this.phone);
+
   @override
   _OTPScreenState createState() => _OTPScreenState();
 }
 
 class _OTPScreenState extends State<OTPScreen> {
+  final _pinPutController = TextEditingController();
+  final _pinPutFocusNode = FocusNode();
+  final BoxDecoration pinPutDecoration = BoxDecoration(
+    color: const Color.fromRGBO(43, 46, 66, 1),
+    borderRadius: BorderRadius.circular(10.0),
+    border: Border.all(
+      color: const Color.fromRGBO(126, 203, 224, 1),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OTP Verification'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Center(
+              child: Text('Verify +91- $widget.phone}',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: PinPut(
+              fieldsCount: 6,
+              withCursor: true,
+              textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+              eachFieldWidth: 40.0,
+              eachFieldHeight: 55.0,
+              // onSubmit: (String pin) => _showSnackBar(pin),
+              focusNode: _pinPutFocusNode,
+              controller: _pinPutController,
+              submittedFieldDecoration: pinPutDecoration,
+              selectedFieldDecoration: pinPutDecoration,
+              followingFieldDecoration: pinPutDecoration,
+              pinAnimationType: PinAnimationType.fade,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
